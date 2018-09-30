8492a57bd3b6421282ed202dd39c2fdcb7100b50
John Papa <john@johnpapa.net>
2018-09-30 12:31:44 -0400

    mod 6

---
+++ src/components/BookForm.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <form action="#" method="post" @submit.prevent="bookSubmit(bookTitle, bookAuthor)">
+      <input type="text" placeholder="Book Title" v-model="bookTitle">
+      <input type="text" placeholder="Book Author" v-model="bookAuthor">
+      <button type="submit" name="button">Add Book</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BookForm',
+  props: ['books'],
+  data() {
+    return {
+      bookTitle: '',
+      bookAuthor: '',
+    };
+  },
+  methods: {
+    bookSubmit(bookTitle, bookAuthor) {
+      this.$emit('addBook', bookTitle, bookAuthor);
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -4,15 +4,17 @@
     <ul>
       <book-item li v-for="book in books" :key="book.title" :book="book"></book-item>
     </ul>
+    <book-form @addBook="appendBook"></book-form>
   </div>
 </template>
 
 <script>
 import BookItem from './BookItem';
+import BookForm from './BookForm';
 
 export default {
   name: 'BookList',
-  components: { BookItem },
+  components: { BookItem, BookForm },
   data() {
     return {
       title: 'All Books',
@@ -22,6 +24,11 @@ export default {
         { title: 'Amusing Ourselves to Death', author: 'Neil Postman' },
       ],
     };
+  },
+  methods: {
+    appendBook(bookTitle, bookAuthor) {
+      this.books.push({ title: bookTitle, author: bookAuthor });
+    },
   },
 };
 </script>
